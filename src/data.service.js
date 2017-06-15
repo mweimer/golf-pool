167d4d611b36a647ddae7a95c6f199f25e34f04c
Matthew L Weimer <mweimer@cas.org>
2017-06-15 13:11:22 -0400

    finally good

--- src/data.service.js
+++ src/data.service.js
@@ -23,7 +23,7 @@ const service = function($http, GOLFERS, CONTESTANTS, MOVEMENT, LEADERBOARD_URL,
 		if (selectedContestantId >= 0) {
 			const positions = entries.filter(e => e.contestantId === selectedContestantId && !e.isDQ)
 				.map(e => e.position)
-				.reduce((c, n) => c + ', ' + n);
+				.reduce((c, n) => c !== null ? c + ', ' + n : n , null);
 
 			$rootScope.positions = positions;
 		} else {
