9ea11c73fa3ccf4089af3632bb151c821a97285a
Matt Weimer <mweimer85@gmail.com>
2017-06-14 22:43:31 -0400

    adding cache busting

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,8 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-          template: 'src/index.html'
+          template: 'src/index.html',
+          hash: true
         })
     ],
     devtool: 'source-map'
