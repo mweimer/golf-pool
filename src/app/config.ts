e89d4756881a700aa993c3ab73c2b523a8e56b6f
Matt Weimer <mweimer85@gmail.com>
2017-07-19 23:00:49 -0400

    new entry

--- src/app/config.ts
+++ src/app/config.ts
@@ -167,9 +167,10 @@ const golferData = [
 const contestantData = [
     { id: 1, name: 'Kevin Donoher', entries: [[2, 13, 41, 86], [4, 16, 37, 74], [6, 30, 33, 88]] },
     { id: 2, name: 'Kyle Bivenour', entries: [[3, 13, 43, 90], [2, 12, 35, 94], [10, 28, 46, 88]] },
-    { id: 3, name: 'Matt Weimer', entries: [[3, 21, 50, 75], [3, 20, 46, 79], [6, 23, 38, 90]] },
-    { id: 4, name: 'Nate Heckmann', entries: [[3, 30, 51, 90], [9, 26, 43, 94], [6, 25, 38, 79]] },
-    { id: 5, name: 'Ryan Boudouris', entries: [[5, 16, 35, 75], [11, 12, 37, 75], [7, 13, 36, 78]] }
+    { id: 3, name: 'Matt Kilianski', entries: [[3, 16, 36, 80], [7, 13, 50, 75], [8, 16, 35, 86]] },
+    { id: 4, name: 'Matt Weimer', entries: [[3, 21, 50, 75], [3, 20, 46, 79], [6, 23, 38, 90]] },
+    { id: 5, name: 'Nate Heckmann', entries: [[3, 30, 51, 90], [9, 26, 43, 94], [6, 25, 38, 79]] },
+    { id: 6, name: 'Ryan Boudouris', entries: [[5, 16, 35, 75], [11, 12, 37, 75], [7, 13, 36, 78]] }
 ];
 
 export { tourneyTitle, tourneyId, golferData, contestantData };
