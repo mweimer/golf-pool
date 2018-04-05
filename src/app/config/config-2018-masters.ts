df3d19ff79aeb93358bf7c84ffab0fcf05d2beca
Matthew L Weimer <mweimer@cas.org>
2018-04-04 22:32:47 -0400

    moar

--- src/app/config/config-2018-masters.ts
+++ src/app/config/config-2018-masters.ts
@@ -114,7 +114,9 @@ const contestantData = [
     { id: 16, name: 'Lorenzo Washington', entries: [[7, 12, 31, 59], [3, 13, 53, 61], [2, 17, 29, 60]] },
     { id: 17, name: 'Matt Bobson', entries: [[3, 12, 34, 55], [7, 15, 34, 57], [4, 12, 31, 55]] },
     { id: 18, name: 'Ryan Aguiar', entries: [[7, 12, 31, 54], [2, 17, 28, 60], [1, 14, 38, 55]] },
-    { id: 19, name: 'Matt Kilianski', entries: [[5, 15, 31, 60], [1, 14, 30, 61], [7, 11, 34, 55]] }
+    { id: 19, name: 'Matt Kilianski', entries: [[5, 15, 31, 60], [1, 14, 30, 61], [7, 11, 34, 55]] },
+    { id: 20, name: 'Brendan McCausland', entries: [[9, 11, 27, 60], [6, 17, 38, 62], [1, 16, 37, 63]] },
+    { id: 21, name: 'Kevin O\'Brien', entries: [[3, 13, 49, 63], [2, 11, 27, 66], [5, 22, 27, 62]] }
 ];
 
 export default { tourneyTitle, tourneyId, golferData, contestantData };
