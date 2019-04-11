fb2369411d6f3a15cbab8ce735d15c3bca2ab6f0
Matthew L Weimer <mweimer@cas.org>
2019-04-11 00:33:24 -0400

    more entries

--- scripts/config-2019-masters-tournament.ts
+++ scripts/config-2019-masters-tournament.ts
@@ -99,7 +99,24 @@ const contestantData = [
     { id: 1, name: 'Matt Weimer', entries: [[1, 11, 33, 58], [4, 14, 35, 53], [5, 20, 30, 59]] },
     { id: 2, name: 'Ryan Boudouris', entries: [[3, 11, 32, 53], [4, 13, 39, 59], [7, 21, 33, 61]] },
     { id: 3, name: 'Lorenzo Washington', entries: [[8, 17, 30, 55], [2, 15, 35, 62], [1, 13, 38, 60]] },
-    { id: 4, name: 'Andrew Kubaszewski', entries: [[3, 21, 39, 62], [6, 12, 41, 60], [8, 17, 35, 59]] }
+    { id: 4, name: 'Andrew Kubaszewski', entries: [[3, 21, 39, 62], [6, 12, 41, 60], [8, 17, 35, 59]] },
+    { id: 5, name: 'Andrew Kaczala', entries: [[6, 11, 49, 58], [3, 19, 34, 75], [7, 27, 44, 64]] },
+    { id: 6, name: 'Nate Heckman', entries: [[3, 12, 39, 62], [1, 16, 34, 53], [6, 21, 33, 56]] },
+    { id: 7, name: 'David Prevo', entries: [[1, 14, 32, 57], [4, 18, 33, 58], [7, 11, 37, 53]] },
+    { id: 8, name: 'Ian Horwich', entries: [[7, 15, 34, 59], [4, 11, 48, 65], [2, 15, 19, 55]] },
+    { id: 9, name: 'Alex Duff', entries: [[4, 11, 51, 68], [1, 21, 34, 60], [10, 17, 30, 54]] },
+    { id: 10, name: 'Kevin O\'Brien', entries: [[1, 18, 34, 58], [2, 20, 33, 55], [7, 12, 31, 53]] },
+    { id: 11, name: 'Matt Dorow', entries: [[4, 12, 38, 62], [1, 11, 50, 53], [4, 15, 36, 75]] },
+    { id: 12, name: 'Dan Godshall', entries: [[1, 17, 38, 59], [3, 11, 34, 59], [5, 19, 30, 59]] },
+    { id: 13, name: 'Matt Kilianski', entries: [[1, 15, 33, 54], [2, 19, 45, 55], [8, 13, 34, 54]] },
+    { id: 14, name: 'Nick Royer', entries: [[3, 20, 30, 61], [5, 24, 30, 63], [9, 26, 31, 56]] },
+    { id: 15, name: 'Michael Nickerson', entries: [[6, 15, 38, 57], [8, 13, 40, 53], [2, 16, 30, 61]] },
+    { id: 16, name: 'Ryan Aguiar', entries: [[6, 20, 34, 60], [1, 21, 33, 53], [7, 16, 31, 55]] },
+    { id: 17, name: 'Max Marshall', entries: [[2, 15, 35, 67], [7, 16, 30, 59], [5, 21, 31, 59]] },
+    { id: 18, name: 'Kevin Donoher', entries: [[1, 11, 38, 57], [6, 12, 36, 53], [3, 21, 39, 59]] },
+    { id: 19, name: 'Kyle Bivenour', entries: [[3, 9, 26, 61], [11, 17, 28, 50], [5, 14, 34, 61]] },
+    { id: 20, name: 'Cameron Weimer', entries: [[1, 16, 38, 63], [3, 15, 32, 63], [4, 21, 31, 59]] },
+    { id: 21, name: 'Drew Serruto', entries: [[1, 13, 33, 61], [3, 11, 30, 57], [6, 14, 38, 63]] }
 ];
 
 export default { tourneyTitle, tourneyId, golferData, contestantData };
