7bbe6518d01162615197c7838c14582e0044fb9a
Matthew L Weimer <mweimer@cas.org>
2019-05-15 20:31:24 -0400

    fix

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -11,8 +11,8 @@ export class AppConfig implements IAppConfig {
     public LEADERBOARD_URL = `https://site.web.api.espn.com/apis/site/v2/sports/golf/leaderboard` +
         `?league=pga&region=us&lang=en&event=${this.config.tourneyId}&showAirings=true`;
 
-    public PLAYER_INFO_URL = `https://site.web.api.espn.com/apis/site/v2/sports/golf/pga/leaderboard' +
-        '/${this.config.tourneyId}/playersummary?player=`;
+    public PLAYER_INFO_URL = `https://site.web.api.espn.com/apis/site/v2/sports/golf/pga/leaderboard` +
+        `/${this.config.tourneyId}/playersummary?player=`;
 
     public TOURNEY_TITLE = this.config.tourneyTitle;
 
